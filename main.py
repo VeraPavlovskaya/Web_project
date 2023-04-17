8e2e4f4aaf2f41dff4af4f931a50db784d0af5d9
pavlovskaya.v <vera.pavlovskya2008@yandex.ru>
2023-04-17 18:25:40 +0300

    ready

---
+++ main.py
@@ -0,0 +1,191 @@
+from flask import Flask, url_for, render_template, redirect, request, abort
+
+# 1. Выполним первоначальную настройку модуля. Сначала импортируем нужный класс:
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+
+from forms.news import NewsForm
+from forms.user import RegisterForm, LoginForm
+from data.news import News
+from data.users import User
+from data import db_session
+
+app = Flask(__name__)
+
+########################################################################################################################
+# 2. Затем сразу после создания приложения flask инициализируем LoginManager:
+login_manager = LoginManager()
+login_manager.init_app(app)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+# 3. Для верной работы flask-login у нас должна быть
+# функция load_user для получения пользователя
+# украшенная декоратором login_manager.user_loader. Добавим ее:
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+# 4. Кроме того, наша модель для пользователей
+# должна содержать ряд методов
+# для корректной работы flask-login,
+# но мы не будем создавать их руками,
+# а воспользуемся множественным наследованием.
+# см. файл: \data\users.py
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+@app.route('/news', methods=['GET', 'POST'])
+@login_required
+def add_news():
+    form = NewsForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        news = News()
+        news.title = form.title.data
+        news.content = form.content.data
+        news.is_private = form.is_private.data
+        current_user.news.append(news)
+        db_sess.merge(current_user)
+        db_sess.commit()
+        return redirect('/notes')
+    return render_template('news.html', title='Добавление новости', form=form)
+
+
+@app.route('/news_delete/<int:id>', methods=['GET', 'POST'])
+@login_required
+def news_delete(id):
+    db_sess = db_session.create_session()
+    news = db_sess.query(News).filter(News.id == id, News.user == current_user).first()
+    if news:
+        db_sess.delete(news)
+        db_sess.commit()
+    else:
+        abort(404)
+    return redirect('/notes')
+
+
+@app.route('/news/<int:id>', methods=['GET', 'POST'])
+@login_required
+def edit_news(id):
+    form = NewsForm()
+    db_sess = db_session.create_session()
+    news = db_sess.query(News).filter(News.id == id, News.user == current_user).first()
+    if request.method == "GET":
+        # db_sess = db_session.create_session()
+        # news = db_sess.query(News).filter(News.id == id, News.user == current_user).first()
+        if news:
+            form.title.data = news.title
+            form.content.data = news.content
+            form.is_private.data = news.is_private
+        else:
+            abort(404)
+    if form.validate_on_submit():
+        # db_sess = db_session.create_session()
+        # news = db_sess.query(News).filter(News.id == id, News.user == current_user).first()
+        if news:
+            news.title = form.title.data
+            news.content = form.content.data
+            news.is_private = form.is_private.data
+            db_sess.commit()
+            return redirect('/notes')
+        else:
+            abort(404)
+    return render_template('news.html', title='Редактирование заметки', form=form)
+
+
+@app.route("/notes")
+def notes():
+    db_sess = db_session.create_session()
+    if current_user.is_authenticated:
+        news = db_sess.query(News).filter((News.user == current_user) | (News.is_private != True))
+    else:
+        news = db_sess.query(News).filter(News.is_private != True)
+    return render_template("notes.html", news=news)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация', form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация', form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+# 7. И, наконец, сделаем обработчик адреса /login:
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # Не забудьте импортировать класс LoginForm и метод login_user из модуля flask-login.
+    form = LoginForm()
+    # Если форма логина прошла валидацию,
+    if form.validate_on_submit():
+        # Создаем сессию для работы БД:
+        db_sess = db_session.create_session()
+        # Находим в БД пользователя по введенной почте:
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        # Проверяем, введен ли для него правильный пароль, если да, вызываем функцию login_user модуля flask-login
+        if user and user.check_password(form.password.data):
+            #  и передаем туда объект нашего пользователя, а также значение галочки «Запомнить меня»:
+            login_user(user, remember=form.remember_me.data)
+            # После чего перенаправляем пользователя на главную страницу нашего приложения:
+            return redirect("/")
+        # Если пароль неправильный:
+        return render_template('login.html', message="Неправильный логин или пароль", form=form)
+    # Если авторизация не пройдена, то возвращаемся на начало авторизации:
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+
+########################################################################################################################
+
+
+
+@app.route('/Subjects')
+###
+def subjects():
+    return render_template("subjects.html", name='subjects')
+@app.route('/Future_works')
+def future_works():
+    return render_template("future_works.html", name='future_works')
+@app.route('/About_me')
+def about_me():
+    return render_template("about_me.html", name='about_me')
+@app.route('/Definitions')
+def definitions():
+    return render_template("definitions.html", name='definitions')
+
+@app.route('/Contur_maps')
+def contur_maps():
+    return render_template("contur_maps.html", name='contur_maps')
+
+@app.route('/')
+@app.route('/Geo_Core')
+def Geo_Core():
+    return render_template("index.html")
+
+def main():
+    db_session.global_init("db/blogs.db")
+    app.run(port=8080, host='127.0.0.1')
+
+if __name__ == '__main__':
+    main()
